4196f147dddfd8f6e5b13d92c38855f20e17d5fc
Ronald-TR <ronald-farias@outlook.com>
2018-04-06 08:23:31 -0300

    adding __init__ for modules import

--- designPatterns/python/singleton/main_singletons.py
+++ designPatterns/python/singleton/main_singletons.py
@@ -1,3 +1,4 @@
-from inModuleSingleton import standard_singleton
+from designPatterns.python.singleton.inModuleSingleton import standard_singleton
 
-print(standard_singleton)
+def get_inmodule():
+    return standard_singleton.__str__()
--- testcases/tests.py
+++ testcases/tests.py
@@ -1,3 +1,4 @@
+from designPatterns.python.singleton import main_singletons
 import unittest
 
 
@@ -5,6 +6,9 @@ class TestBriefCase(unittest.TestCase):
 
     def test_assert(self):
         self.assertEqual(1, 1)
+    
+    def test_singletons(self):
+        self.assertEqual('Calling the singleton module!!', main_singletons.get_inmodule())
 
 
 if __name__ == '__main__':
