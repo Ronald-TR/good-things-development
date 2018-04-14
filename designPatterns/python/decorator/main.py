bcadcab3f9cf4aedb3ff1f463e3f71eda915a3e2
Ronald-TR <ronald-farias@outlook.com>
2018-04-14 11:40:53 -0300

    fixed codestyle pylama

--- designPatterns/python/decorator/main.py
+++ designPatterns/python/decorator/main.py
@@ -1,16 +1,20 @@
 from designPatterns.python.decorator.decorator import hello_decorator
 
+
 def echo_name_01(name):
     return f"Your name: {name}"
+
 
 # calling without sugar sintax @ for decorators
 name = hello_decorator(echo_name_01)('Ronald')
 print(name)
 
+
 @hello_decorator
 def echo_name_02(name):
     return f"Your name: {name}"
 
-#calling with the sugar sintax, much better... right?
+
+# calling with the sugar sintax, much better... right?
 name = echo_name_02('Ronald')
 print(name)
